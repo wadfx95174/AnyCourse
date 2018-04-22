c9d71007ef2e58a073952f8d13e8037d385e2f67
isr10132 <isr10132@gmail.com>
2018-04-23 02:18:00 +0800

    add YoutubePlayer js
    combine jw & yt player in playerInterface

---
+++ AnyCourse/src/PlayerInterface/Unit.java
@@ -0,0 +1,69 @@
+package PlayerInterface;
+
+public class Unit
+{
+	private int unitId;
+	private String schoolName;
+	private String listName;
+	private String unitName;
+	private String videoUrl;
+	private int likes;
+	private String videoImgSrc;
+	public int getUnitId()
+	{
+		return unitId;
+	}
+	public void setUnitId(int unitId)
+	{
+		this.unitId = unitId;
+	}
+	public String getSchoolName()
+	{
+		return schoolName;
+	}
+	public void setSchoolName(String schoolName)
+	{
+		this.schoolName = schoolName;
+	}
+	public String getListName()
+	{
+		return listName;
+	}
+	public void setListName(String listName)
+	{
+		this.listName = listName;
+	}
+	public String getUnitName()
+	{
+		return unitName;
+	}
+	public void setUnitName(String unitName)
+	{
+		this.unitName = unitName;
+	}
+	public String getVideoUrl()
+	{
+		return videoUrl;
+	}
+	public void setVideoUrl(String videoUrl)
+	{
+		this.videoUrl = videoUrl;
+	}
+	public int getLikes()
+	{
+		return likes;
+	}
+	public void setLikes(int likes)
+	{
+		this.likes = likes;
+	}
+	public String getVideoImgSrc()
+	{
+		return videoImgSrc;
+	}
+	public void setVideoImgSrc(String videoImgSrc)
+	{
+		this.videoImgSrc = videoImgSrc;
+	}
+	
+}
