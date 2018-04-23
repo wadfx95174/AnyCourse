37270216b3e13a427d846c381202eec42d1b665a
wadfx95174 <wadfx95174@gmail.com>
2018-04-23 09:29:12 +0800

    VideoList to Playerface Debug

--- AnyCourse/src/Personal/VideoList/VideoListDatabaseManager.java
+++ AnyCourse/src/Personal/VideoList/VideoListDatabaseManager.java
@@ -96,8 +96,9 @@ public class VideoListDatabaseManager {
 					unitVideo.setOorder(result.getInt("customlist_video.oorder"));
 					unitVideo.setLikes(result.getInt("unit.likes"));
 					unitVideo.setUnitID(result.getInt("customlist_video.unit_id"));
-					
-					if(result.getString("unit.video_url").split("\\.")[2].equals("youtube")) {
+					System.out.println(result.getString("unit.video_url"));
+					System.out.println(result.getString("unit.video_url").split("/")[2]);
+					if(result.getString("unit.video_url").split("/")[2].equals("www.youtube.com")) {
 //						System.out.println(result.getString("unit.video_url"));
 						unitVideo.setVideoType(1);//youtube
 					}
@@ -105,6 +106,7 @@ public class VideoListDatabaseManager {
 //						System.out.println(result.getString("unit.video_url"));
 						unitVideo.setVideoType(2);//jwplayer
 					}
+					System.out.println(unitVideo.getVideoType());
 					unitVideos.add(unitVideo);
 				}
 			} 
