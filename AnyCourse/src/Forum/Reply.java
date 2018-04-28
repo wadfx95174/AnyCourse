ad05380e0c64d716c4ba19cf658dd1a9d99102e9
jerry80088 <33078522+jerry80088@users.noreply.github.com>
2018-04-28 12:08:32 +0800

    Forum

---
+++ AnyCourse/src/Forum/Reply.java
@@ -0,0 +1,40 @@
+package Forum;
+
+public class Reply {
+	public int reply_id;
+	public int comment_id;
+	public String user_id;
+	public String reply_time;
+	public String reply_content;
+	
+	public int getReply_id() {
+		return reply_id;
+	}
+	public void setReply_id(int reply_id) {
+		this.reply_id = reply_id;
+	}
+	public int getComment_id() {
+		return comment_id;
+	}
+	public void setComment_id(int comment_id) {
+		this.comment_id = comment_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getReply_time() {
+		return reply_time;
+	}
+	public void setReply_time(String reply_time) {
+		this.reply_time = reply_time;
+	}
+	public String getReply_content() {
+		return reply_content;
+	}
+	public void setReply_content(String reply_content) {
+		this.reply_content = reply_content;
+	}	
+}
