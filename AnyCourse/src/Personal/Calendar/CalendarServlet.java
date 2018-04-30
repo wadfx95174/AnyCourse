2d99651f484d71715e0f5c2553c27acab0a5cde0
isr10132 <isr10132@gmail.com>
2018-05-01 02:57:28 +0800

    update Calendar
    Connect with database
    
    set coursePlan to the area

--- AnyCourse/src/Personal/Calendar/CalendarServlet.java
+++ AnyCourse/src/Personal/Calendar/CalendarServlet.java
@@ -1,13 +1,17 @@
 package Personal.Calendar;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.google.gson.Gson;
+
+import Personal.CoursePlan.CoursePlanManager;
 
 public class CalendarServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -16,7 +20,17 @@ public class CalendarServlet extends HttpServlet {
 		CalendarManager calendarManager = new CalendarManager();
 		response.setContentType("application/json");
 		response.setCharacterEncoding("UTF-8");
-		response.getWriter().write(calendarManager.getEvents());
+		HttpSession session = request.getSession();
+		String userId = (String)session.getAttribute("userId");
+		if (request.getParameter("method").equals("getEvent"))
+		{
+			response.getWriter().write(new Gson().toJson(calendarManager.getEvents(userId)));
+		}
+		else if (request.getParameter("method").equals("getCoursePlan"))
+		{
+			CoursePlanManager coursePlanManager = new CoursePlanManager();
+			response.getWriter().write(new Gson().toJson(coursePlanManager.getCoursePlanAllList(userId)));
+		}
 	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -33,7 +47,8 @@ public class CalendarServlet extends HttpServlet {
 		response.setCharacterEncoding("UTF-8");
 		if (request.getParameter("method").equals("insert"))
 		{
-			int newId = calendarManager.insertEvent(event);
+			HttpSession session = request.getSession();
+			int newId = calendarManager.insertEvent(event, (String)session.getAttribute("userId"));
 			String json = new Gson().toJson(newId);
 			response.getWriter().write(json);
 		}
