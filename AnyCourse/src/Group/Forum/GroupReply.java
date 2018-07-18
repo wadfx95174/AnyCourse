020c4afd7d18b8549e9a9f78b2545631999d279f
jerry80088 <33078522+jerry80088@users.noreply.github.com>
2018-07-19 03:16:16 +0800

    GroupForum

---
+++ AnyCourse/src/Group/Forum/GroupReply.java
@@ -0,0 +1,47 @@
+package Group.Forum;
+
+public class GroupReply {
+	public int replyId;
+	public int commentId;
+	public String userId;
+	public String nickName;	
+	public String replyTime;
+	public String replyContent;
+	public int getReplyId() {
+		return replyId;
+	}
+	public void setReplyId(int replyId) {
+		this.replyId = replyId;
+	}
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getReplyTime() {
+		return replyTime;
+	}
+	public void setReplyTime(String replyTime) {
+		this.replyTime = replyTime;
+	}
+	public String getReplyContent() {
+		return replyContent;
+	}
+	public void setReplyContent(String replyContent) {
+		this.replyContent = replyContent;
+	}
+	
+}
---
+++ AnyCourse/src/Group/Forum/GroupReplyServlet.java
@@ -0,0 +1,129 @@
+package Group.Forum;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+import Group.Forum.GroupForumManager;
+import Group.Forum.GroupReply;
+
+/**
+ * Servlet implementation class GroupReplyServlet
+ */
+public class GroupReplyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GroupReplyServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		GroupForumManager dbreply = new GroupForumManager();
+		response.setHeader("Cache-Control","max-age=0");
+		ArrayList<GroupReply> replys = new ArrayList<GroupReply>();
+
+		dbreply.selectReplyTable(replys);
+		String replyJson = new Gson().toJson(replys);
+		response.setContentType("application/json;charset = utf-8;");
+		response.getWriter().write(replyJson);
+		dbreply.conClose();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+		String state = request.getParameter("state");
+		response.setHeader("Cache-Control","max-age=0");
+		if(state.equals("insert"))
+		{
+			int commentId = Integer.parseInt(request.getParameter("commentId"));
+			String userId = (String)session.getAttribute("userId");
+			String nickName = (String)session.getAttribute("nickName");
+			String replyContent = request.getParameter("replyContent");
+			
+			GroupForumManager dbreply = new GroupForumManager();
+			
+			GroupReply reply = new GroupReply();
+			
+			reply.setCommentId(commentId);
+			reply.setUserId(userId);
+			reply.setNickName(nickName);
+			reply.setReplyContent(replyContent);
+
+			reply = dbreply.insertReplyTable(reply);
+			String replyJson = new Gson().toJson(reply);
+			response.setContentType("application/json;charset = utf-8;");
+			response.getWriter().write(replyJson);
+			dbreply.conClose();
+		}
+		if(state.equals("update"))
+		{
+			int replyId = Integer.parseInt(request.getParameter("replyId"));
+			int commentId = Integer.parseInt(request.getParameter("commentId"));
+			String userId = (String)session.getAttribute("userId");
+			String nickName = (String)session.getAttribute("nickName");
+			String replyTime = request.getParameter("replyTime");
+			String replyContent = request.getParameter("replyContent");
+			
+			GroupForumManager dbreply = new GroupForumManager();
+			
+			GroupReply reply = new GroupReply();
+			
+			reply.setReplyId(replyId);
+			reply.setCommentId(commentId);
+			reply.setUserId(userId);
+			reply.setNickName(nickName);
+			reply.setReplyTime(replyTime);
+			reply.setReplyContent(replyContent);
+
+			dbreply.updateReplyTable(reply);
+			PrintWriter out = response.getWriter();		
+			out.print("success");
+			dbreply.conClose();
+		}	
+		if(state.equals("delete"))
+		{
+			GroupForumManager dbreply = new GroupForumManager();
+			int replyId = Integer.parseInt(request.getParameter("replyId"));
+			
+			GroupReply reply = new GroupReply();
+			reply.setReplyId(replyId);
+						
+			dbreply.deleteReplyTable(replyId);
+			PrintWriter out = response.getWriter();
+			dbreply.conClose();
+		}	
+		if(state.equals("delete2"))
+		{
+			GroupForumManager dbreply = new GroupForumManager();
+			int commentId = Integer.parseInt(request.getParameter("commentId"));
+			
+			GroupReply reply = new GroupReply();
+			reply.setCommentId(commentId);
+						
+			dbreply.deleteReplyTable2(commentId);
+			dbreply.conClose();
+		}	
+	}
+
+}
