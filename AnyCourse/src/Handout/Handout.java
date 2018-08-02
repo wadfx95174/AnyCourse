44fe20eb04c836de5935a01228222ff5826ddf1d
jerry80088 <33078522+jerry80088@users.noreply.github.com>
2018-08-02 12:55:42 +0800

    handout

---
+++ AnyCourse/src/Handout/Handout.java
@@ -0,0 +1,26 @@
+package Handout;
+
+public class Handout {
+	public int unitId;
+	public String handoutName;
+	public String handoutUrl;
+	public int getUnitId() {
+		return unitId;
+	}
+	public void setUnitId(int unitId) {
+		this.unitId = unitId;
+	}
+	public String getHandoutName() {
+		return handoutName;
+	}
+	public void setHandoutName(String handoutName) {
+		this.handoutName = handoutName;
+	}
+	public String getHandoutUrl() {
+		return handoutUrl;
+	}
+	public void setHandoutUrl(String handoutUrl) {
+		this.handoutUrl = handoutUrl;
+	}
+	
+}
