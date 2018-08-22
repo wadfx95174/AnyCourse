4ccf3cb9c7d3797fa75fd6f69f25e11bdeadb2fc
jerry80088 <33078522+jerry80088@users.noreply.github.com>
2018-08-22 08:10:55 +0800

    GroupNote
    後端差不多都好了，前端還沒好，前端於個人筆記頁面點選按鈕分享至某群組(類似新增至課程清單那樣)按鈕還沒加上去，群組筆記頁面預計跟個人筆記呈現差不多

---
+++ AnyCourse/src/Group/Note/GroupTextNoteServlet.java
@@ -0,0 +1,49 @@
+package Group.Note;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.gson.Gson;
+
+
+
+
+public class GroupTextNoteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GroupTextNoteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		GroupNoteManager dbgroupNote = new GroupNoteManager();	
+		HttpSession session = request.getSession();
+		
+		response.setContentType("application/json;charset = utf-8;");
+		response.getWriter().write(dbgroupNote.selectGroupTextNoteTable(Integer.parseInt(request.getParameter("groupId"))));	
+		dbgroupNote.conClose();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
